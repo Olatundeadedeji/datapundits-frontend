33dbb9034851664cfef50ba38d0672941fe227c6
Olatunde  Adedeji <olatundeadedeji32@gmail.com>
2024-08-27 23:53:21 +0100

    coursedetails component added

---
+++ src/components/CourseDetails.vue
@@ -0,0 +1,40 @@
+<template> 
+    <div> 
+      <h1>Course Details</h1> 
+      <p v-if="course">Course Name: {{ course.name }}</p> 
+      <p v-else>Select a course to see details</p> 
+    </div> 
+  </template> 
+   
+  <script> 
+  import { ref, watch } from 'vue'; 
+   
+  export default { 
+    props: { 
+      selectedCourse: { 
+        type: Object, 
+        required: false 
+      } 
+    }, 
+    setup(props) { 
+      const course = ref(null); 
+   
+     
+      watch(() => props.selectedCourse, (newCourse) => { 
+        course.value = newCourse; 
+      }); 
+   
+      return { 
+        course 
+      }; 
+    } 
+  }; 
+  </script> 
+   
+  <style scoped> 
+  p { 
+    font-size: 16px; 
+    color: gray; 
+  } 
+  </style> 
+  
