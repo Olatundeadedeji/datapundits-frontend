9fadfb8984fb13182c3bf4320593fdddf4b80be2
Olatunde  Adedeji <olatundeadedeji32@gmail.com>
2024-08-28 00:00:10 +0100

    App component updated

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,49 @@
-<script setup>
-import {ref} from "vue"
-const msg = ref("Hello Datapundits!")
+<template>
+  <div id="app">
+    <h1>Datapundits: Learning Management System</h1>
+
+    <UserProfile />
+
+
+    <CourseList @select="selectCourse" />
+    <CourseDetails :course="selectedCourse" />
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+import UserProfile from './components/UserProfile.vue';
+import CourseList from './components/CourseList.vue';
+import CourseDetails from './components/CourseDetails.vue';
+
+export default {
+  components: {
+    UserProfile,
+    CourseList,
+    CourseDetails,
+  },
+  setup() {
+
+    const selectedCourse = ref(null);
+
+
+    const selectCourse = (course) => {
+      selectedCourse.value = course;
+    };
+
+    return {
+      selectedCourse,
+      selectCourse,
+    };
+  },
+};
 </script>
 
-<template>
-  <h1>{{ msg }}</h1>
-</template>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
